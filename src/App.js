6f5e5d6edeaf4a0fcfedd474eec90a1f343d7de4
Nitesh2905 <nkyadav2905@gmail.com>
2022-06-22 00:01:26 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import Header from "./component/Header";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./component/Homepage";
+import CoinPage from "./component/CoinPage";
+import { makeStyles } from "@material-ui/core";
 
 function App() {
   return (
     <Router>
-      <div>
+      <div className="app">
         <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/coins/:id" element={<CoinPage />} />
+        </Routes>
       </div>
     </Router>
   );
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,10 +1,70 @@
+import {
+  AppBar,
+  Container,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+  makeStyles,
+  ThemeProvider,
+  createTheme,
+} from "@material-ui/core";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../csscomponent/Header.css";
+
+const useStyles = makeStyles(() => ({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+}));
 
 const Header = () => {
+  const classes = useStyles();
+
+  const navigate = useNavigate();
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div>
+        <AppBar position="static" color="transparent">
+          <Container>
+            <Toolbar>
+              <Typography
+                variant="h6"
+                className={classes.title}
+                onClick={() => navigate("/")}
+              >
+                Track Crypto
+              </Typography>
+              <Select
+                variant="outlined"
+                style={{
+                  width: 100,
+                  height: 40,
+                  marginRight: 15,
+                }}
+              >
+                <MenuItem value={"USD"}>USD</MenuItem>
+                <MenuItem value={"INR"}>INR</MenuItem>
+              </Select>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </div>
+    </ThemeProvider>
   );
 };
 
